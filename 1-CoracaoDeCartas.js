1866cfb54068a16faa044a0a39e837ec69c99ea9
Riller <rvsriller@gmail.com>
2021-05-01 23:18:52 -0300

    Desafio 1: Coração de Cartas

--- 1-CoracaoDeCartas.js
+++ 1-CoracaoDeCartas.js
@@ -1,20 +1,21 @@
 /*
-O coração das cartas, como Marcos apelidou o jogo, é individual e jogado com três pilhas, 
-inicialmente com o mesmo número de cartas. Cada carta tem um valor numérico inteiro de 0 até 9. 
-O jogador pode, a qualquer momento ver o valor de qualquer carta, mas só pode jogar com as 
-cartas que estão no topo das pilhas. Em cada rodada, o jogador pode remover qualquer combinação 
-de cartas que estejam no topo da pilha (pode escolher 1, 2 ou até 3 cartas) cuja soma dos valores 
-seja múltiplo de 3. O jogo é ganho quando todas as cartas forem removidas das pilhas. 
-Se alguma carta não puder ser removida, perde-se o jogo.
+  O coração das cartas, como Marcos apelidou o jogo, é individual e jogado com três pilhas, 
+  inicialmente com o mesmo número de cartas. Cada carta tem um valor numérico inteiro de 0 até 9. 
+  O jogador pode, a qualquer momento ver o valor de qualquer carta, mas só pode jogar com as 
+  cartas que estão no topo das pilhas. Em cada rodada, o jogador pode remover qualquer combinação 
+  de cartas que estejam no topo da pilha (pode escolher 1, 2 ou até 3 cartas) cuja soma dos valores 
+  seja múltiplo de 3. O jogo é ganho quando todas as cartas forem removidas das pilhas. 
+  Se alguma carta não puder ser removida, perde-se o jogo.
 
-Entrada
-A entrada é composta por várias instâncias Cada instância é iniciada por um inteiro N (0 ≤ N ≤ 100), 
-que identifica o número de cartas em cada pilha. A entrada termina quando N = 0. 
-Cada uma das N linhas seguintes contém três inteiros A, B e C, que descrevem os valores numéricos das 
-cartas em um nível da pilha (0 ≤ A, B, C ≤  9). As pilhas são descritas do topo até o fundo.
+  Entrada
+  A entrada é composta por várias instâncias Cada instância é iniciada por um inteiro N (0 ≤ N ≤ 100), 
+  que identifica o número de cartas em cada pilha. A entrada termina quando N = 0. 
+  Cada uma das N linhas seguintes contém três inteiros A, B e C, que descrevem os valores numéricos das 
+  cartas em um nível da pilha (0 ≤ A, B, C ≤  9). As pilhas são descritas do topo até o fundo.
 
-Saída
-Para cada instância, imprima uma linha contendo o número 1 se o jogador pode ganhar a instância do jogo ou o número 0 se o jogo for impossível.
+  Saída
+  Para cada instância, imprima uma linha contendo o número 1 se o jogador pode ganhar a instância do jogo 
+  ou o número 0 se o jogo for impossível.
 
 */
 
@@ -30,7 +31,10 @@ while (true) {
   cards = Array.from(new Array(3)).map(v => new Array(n));
 
   for (let i = 0; i < n; i++) {
-    [cards[0][i], cards[1][i], cards[2][i]] = gets().match(new RegExp(/\d+/, 'g')).map(v => parseInt(v));
+    [cards[0][i], cards[1][i], cards[2][i]] = 
+    gets().
+    match(new RegExp(/\d+/, 'g')).
+    map(v => parseInt(v));
   }
 
   console.log(canWinWith(0, 0, 0) ? 1 : 0);
