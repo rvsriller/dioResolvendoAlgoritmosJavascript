c5522893f241341660b4c9954d30aea9185a1332
Riller <rvsriller@gmail.com>
2021-05-01 23:22:43 -0300

    Desafio 1: Coração de Cartas

--- 1-CoracaoDeCartas.js
+++ 1-CoracaoDeCartas.js
@@ -19,18 +19,18 @@
 */
 
 let m, cards;
-let n = 1;
+let nLinhas = 1;
 
 while (true) {
-  n = parseInt(gets());
+  nLinhas = parseInt(gets());
 
-  if (n <= 0) break;
+  if (nLinhas <= 0) break;
 
   m = new Map();
 
-  cards = Array.from(new Array(3)).map(v => new Array(n));
+  cards = Array.from(new Array(3)).map(v => new Array(nLinhas));
 
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < nLinhas; i++) {
     [cards[0][i], cards[1][i], cards[2][i]] = 
     gets().
     match(new RegExp(/\d+/, 'g')).
@@ -47,7 +47,7 @@ function canWinWith(a, b, c) {
   s = s.concat(b + '0');
   s = s.concat(c + '0');
 
-  if (a === b && b === c && c === n) {
+  if (a === b && b === c && c === nLinhas) {
     m.set(s, 1);
     return true;
   }
@@ -58,37 +58,37 @@ function canWinWith(a, b, c) {
     return x === 1 ? true : false;
   }
 
-  if (a < n && cards[0][a] % 3 === 0 && canWinWith(a + 1, b, c)) {
+  if (a < nLinhas && cards[0][a] % 3 === 0 && canWinWith(a + 1, b, c)) {
     m.set(s, 1);
     return true;
   }
 
-  if (b < n && cards[1][b] % 3 === 0 && canWinWith(a, b + 1, c)) {
+  if (b < nLinhas && cards[1][b] % 3 === 0 && canWinWith(a, b + 1, c)) {
     m.set(s, 1);
     return true;
   }
 
-  if (c < n && cards[2][c] % 3 === 0 && canWinWith(a, b, c + 1)) {
+  if (c < nLinhas && cards[2][c] % 3 === 0 && canWinWith(a, b, c + 1)) {
     m.set(s, 1);
     return true;
   }
 
-  if (a < n && b < n && (cards[0][a] + cards[1][b]) % 3 == 0 && canWinWith(a + 1, b + 1, c)) {
+  if (a < nLinhas && b < nLinhas && (cards[0][a] + cards[1][b]) % 3 == 0 && canWinWith(a + 1, b + 1, c)) {
     m[s] = 1;
     return true;
   }
 
-  if (a < n && c < n && (cards[0][a] + cards[2][c]) % 3 == 0 && canWinWith(a + 1, b, c + 1)) {
+  if (a < nLinhas && c < nLinhas && (cards[0][a] + cards[2][c]) % 3 == 0 && canWinWith(a + 1, b, c + 1)) {
     m.set(s, 1);
     return true;
   }
   
-  if (b < n && c < n && (cards[1][b] + cards[2][c]) % 3 == 0 && canWinWith(a, b + 1, c + 1)) {
+  if (b < nLinhas && c < nLinhas && (cards[1][b] + cards[2][c]) % 3 == 0 && canWinWith(a, b + 1, c + 1)) {
     m.set(s, 1);
     return true;
   }
 
-  if (a < n && b < n && c < n && (cards[0][a] + cards[1][b] + cards[2][c]) % 3 == 0 && canWinWith(a + 1, b + 1, c + 1)) {
+  if (a < nLinhas && b < nLinhas && c < nLinhas && (cards[0][a] + cards[1][b] + cards[2][c]) % 3 == 0 && canWinWith(a + 1, b + 1, c + 1)) {
     m.set(s, 1);
     return true;
   }
