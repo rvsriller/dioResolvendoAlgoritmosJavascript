0602fe460f384df2370b63b970a13767ea2329df
Riller <rvsriller@gmail.com>
2021-05-02 23:38:32 -0300

    Desafio 2: Abreviando Posts Do Blog

--- 2-AbreviandoPostsDoBlog.js
+++ 2-AbreviandoPostsDoBlog.js
@@ -30,8 +30,8 @@ while (true) {
     const repeatations = sentence.join(' ').match(new RegExp(`\\b${word}\\b`, 'g')).length;
     const trim = word.length - 2;
     const save = trim * repeatations;
-    const abbreviation = `${word[0]}.`
-    const legend = `${abbreviation} = ${word}`
+    const abbreviation = `${word[0]}.`;
+    const legend = `${abbreviation} = ${word}`;
     const regexp = new RegExp(`\\b${word}\\b`, 'g')
 
     return { word, repeatations, trim, save, abbreviation, legend, regexp };
