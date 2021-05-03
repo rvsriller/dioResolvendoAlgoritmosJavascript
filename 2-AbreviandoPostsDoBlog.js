5b334419f23db8cccae07c1b9bf5100731875d8c
Riller <rvsriller@gmail.com>
2021-05-02 23:34:57 -0300

    Desafio 2: Abreviando Posts Do Blog

--- 2-AbreviandoPostsDoBlog.js
+++ 2-AbreviandoPostsDoBlog.js
@@ -1,13 +1,14 @@
 /*
-Entrada
-Haverá diversos casos de teste. Cada caso de teste é composto de uma linha, contendo uma frase de até 10⁴ caracteres. A frase é composta de palavras e espaços em branco, e cada palavra é composta de letras minúsculas ('a'-'z'), e contém entre 1 e 30 caracteres cada.
+  Entrada
+  Haverá diversos casos de teste. Cada caso de teste é composto de uma linha, contendo uma frase de até 10⁴ caracteres. 
+  A frase é composta de palavras e espaços em branco, e cada palavra é composta de letras minúsculas ('a'-'z'), e contém entre 1 e 30 caracteres cada.
 
-O último caso de teste é indicado quando a linha dada conter apenas um “.”, o qual não deverá ser processado.
+  O último caso de teste é indicado quando a linha dada conter apenas um “.”, o qual não deverá ser processado.
 
-Saída
-Para cada caso de teste, imprima uma linha contendo a frase já com as abreviações escolhidas e aplicadas.
+  Saída
+  Para cada caso de teste, imprima uma linha contendo a frase já com as abreviações escolhidas e aplicadas.
 
-Em seguida, imprima um inteiro N, indicando o número de palavras em que foram escolhidas uma letra para a abreviação no texto. Nas próximas N linhas, imprima o seguinte padrão “C. = P”, onde C é a letra inicial e P é a palavra escolhida para tal letra. As linhas devem ser impressas em ordem crescente da letra inicial.
+  Em seguida, imprima um inteiro N, indicando o número de palavras em que foram escolhidas uma letra para a abreviação no texto. Nas próximas N linhas, imprima o seguinte padrão “C. = P”, onde C é a letra inicial e P é a palavra escolhida para tal letra. As linhas devem ser impressas em ordem crescente da letra inicial.
 
 */
 
